8ba85381026f70628a89b7908846e6065490813a
Ben Bangert <ben@groovie.org>
2012-01-30 15:34:42 -0800

    Make setup.py a bit nicer

--- setup.py
+++ setup.py
@@ -21,28 +21,30 @@ if not on_rtd:
         "zkpython>=0.4",
     ])
 
-setup(name='zktools',
-      version=__version__,
-      description='Zookeeper Tools',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name='zktools',
+    version=__version__,
+    description='Zookeeper Tools',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        ],
-      keywords='zookeeper lock leader configuration',
-      author="Ben Bangert",
-      author_email="bbangert@mozilla.com",
-      url="http://zktools.readthedocs.org/",
-      license="MPL2",
-      packages=find_packages(),
-      test_suite="zktools.tests",
-      include_package_data=True,
-      zip_safe=False,
-      tests_require=['pkginfo', 'Mock>=0.7', 'nose'],
-      install_requires=reqs,
-      entry_points="""
-      [console_scripts]
-      zooky = zktools.locking:lock_cli
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    ],
+    keywords='zookeeper lock leader configuration',
+    author="Ben Bangert",
+    author_email="bbangert@mozilla.com",
+    url="http://zktools.readthedocs.org/",
+    license="MPLv2.0",
+    packages=find_packages(),
+    test_suite="zktools.tests",
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=['pkginfo', 'Mock>=0.7', 'nose'],
+    install_requires=reqs,
+    entry_points="""
+    [console_scripts]
+    zooky = zktools.locking:lock_cli
 
-      """
+    """
 )
