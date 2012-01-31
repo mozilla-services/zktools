6856ae096f411a6304019e505ba3ded47c4b0dde
Ben Bangert <ben@groovie.org>
2012-01-30 16:16:02 -0800

    doc update

--- docs/api/node.rst
+++ docs/api/node.rst
@@ -9,6 +9,13 @@ Node Class
 ----------
 
 .. autoclass:: ZkNode
+
+    .. autoattribute:: last_modified
+
+        The last time a :class:`ZkNode` has been modified either by
+        the user or due to a Zookeeper update, recorded as seconds since
+        epoch.
+
     :members: __init__, add_children_subscriber, add_data_subscriber, children, value, connected
 
 .. autoclass:: ZkNodeDict
--- zktools/node.py
+++ zktools/node.py
@@ -146,10 +146,6 @@ class ZkNode(object):
         if its not found will automatically create a blank string as
         the value.
 
-        The last time a :class:`ZkNode` has been modified either by
-        the user or due to a Zookeeper update is recorded as the
-        :attribute:`ZkNode.last_modified` attribute.
-
         :param connection: zookeeper connection object
         :type connection: ZkConnection instance
         :param path: Path to the Zookeeper node
